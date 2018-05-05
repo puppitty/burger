18f42a5285c5997e95268b6ff376ff11e6370c8e
puppitty <33668867+puppitty@users.noreply.github.com>
2018-05-04 21:11:50 -0400

    Initial Files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
+INSERT INTO cats (name) VALUES ('Blue Cheese Burger');
+INSERT INTO cats (name, sleepy) VALUES ('Turkey Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Tofu Burger', true);
